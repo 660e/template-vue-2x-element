d3db2c87490ce5242545c06e2c601cb7a4fe4979
Min <solunar@vip.qq.com>
2020-11-18 16:43:37 +0800

    init filters

--- src/views/demo/filters.vue
+++ src/views/demo/filters.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>filters</h1>
+  <h1>{{ n }} -> {{ n|toThousandFilter }}</h1>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      n: 1234567890
+    };
+  }
+};
+</script>
