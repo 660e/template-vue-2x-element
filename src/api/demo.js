5f25b99c8e590539beedee16244d55f4a17968b4
Min <solunar@vip.qq.com>
2020-11-19 10:55:23 +0800

    init mock

--- src/api/demo.js
+++ src/api/demo.js
@@ -1,4 +1,5 @@
 import request from '@/utils/request.js';
+import mockjs from 'mockjs';
 
 const demoApi = {
 
@@ -8,6 +9,34 @@ const demoApi = {
         results
       }
     });
+  },
+
+  getMockData1() {
+    const response = mockjs.mock({
+      'A|1-100': 1,
+      'B|1-100.1-2': 1,
+      'C|1': true,
+      'D|2': [{ a: 1, b: 2 }],
+      'E': '@string',
+      'F': '@date',
+      'G': '@time',
+      'H': '@title',
+      'I': '@name',
+      'J': '@domain',
+      'K': '@email',
+      'L': '@ip',
+      'M': '@guid',
+      'N': '@ctitle',
+      'O': '@csentence',
+      'P': '@region',
+      'Q': '@province',
+      'R': '@city'
+    });
+    return new Promise(resolve => resolve(response));
+  },
+
+  getMockData2() {
+    return request.get('/demo');
   }
 
 };
