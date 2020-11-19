1b32f256c0eecb17aeed7a825fb2af41a4ee59bf
Min <solunar@vip.qq.com>
2020-11-19 10:22:10 +0800

    init i18n

--- src/views/demo/i18n.vue
+++ src/views/demo/i18n.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>i18n</h1>
+  <div>
+    <el-button @click="changeLanguage('zh')">中文</el-button>
+    <el-button @click="changeLanguage('en')">English</el-button>
+    <h1>{{ $t('demo.hello') }}</h1>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    changeLanguage(lang) {
+      this.$i18n.locale = lang;
+      window.localStorage.setItem('language', lang);
+      window.console.log(this.$i18n.getLocaleMessage(lang).el);
+      window.console.log(this.$i18n.getLocaleMessage(lang).demo);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+h1 {
+  padding-top: 20px;
+}
+</style>
