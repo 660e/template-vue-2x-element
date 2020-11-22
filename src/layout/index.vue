aa0f70b91cc23391cebb43cec4a52d2a7507ff83
Min <solunar@vip.qq.com>
2020-11-22 22:36:27 +0800

    init keep-alive

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -6,7 +6,10 @@
       </el-menu>
     </el-aside>
     <el-main>
-      <router-view></router-view>
+      <keep-alive>
+        <router-view v-if="$route.meta.keepAlive"></router-view>
+      </keep-alive>
+      <router-view v-if="!$route.meta.keepAlive"></router-view>
     </el-main>
   </el-container>
 </template>
---
+++ src/views/demo/keep-alive.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-form label-width="80px">
+    <el-form-item label="参数A">
+      <el-input v-model="form.a" clearable></el-input>
+    </el-form-item>
+    <el-form-item label="参数B">
+      <el-input v-model="form.b" clearable></el-input>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        a: '',
+        b: ''
+      }
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.el-form {
+  width: 300px;
+}
+</style>
